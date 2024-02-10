850e7f9f05a92f2a7bad9d6a31363ef824d0d969
--global <fittleradam@gmail.com>
2024-02-11 00:57:04 +1000

    Navbar basically completed onto the sidebar now

--- firstapp/src/components/Navbar/Navbar.jsx
+++ firstapp/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material'
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import useStyles from './styles'
+import { useTheme } from '@mui/material/styles'
 
 const Navbar = () => {
-    console.log('Navbar')
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width:600px)');
+    const theme = useTheme();
+    const isAuthenticated = true;
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+            <AppBar position="fixed">
+                <Toolbar className={classes.toolbar}>
+                    {isMobile && (
+                        <IconButton 
+                            color="inherit"
+                            edge="start"
+                            style={{ outline: 'none'}}
+                            onClick={() => {}}
+                            className={classes.menuButton}
+                        >
+                            <Menu />
+                        </IconButton>
+                    )}
+                    <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+                        {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />} 
+                    </IconButton>
+                    {!isMobile && 'Search...'}
+                    <div>
+                        {!isAuthenticated ? (
+                            <Button color="inherit" onClick={() => {}} >
+                                Login &nbsp; <AccountCircle />
+                            </Button>
+                        ) : (
+                            <Button 
+                                color="inherit"
+                                component={Link}
+                                to={`/profile/:id`}
+                                className={classes.linkButton}
+                                onClick={() => {}}
+                                >
+                                {!isMobile && <>My Movies &nbsp;</>}
+                                <Avatar 
+                                    style={{width: 30, height: 30}}
+                                    alt="Profile"
+                                    src={'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw2O_baMroSHT6o-lbwB3zkD&ust=1707663087427000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCvm_GCoYQDFQAAAAAdAAAAABAE'}
+                                />
+                            </Button>
+                        )}
+                    </div>
+                    {isMobile && 'Search...'}
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
