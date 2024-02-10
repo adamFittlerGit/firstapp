337cdb70048acd5ad82c44d134e9f1c75bb18317
--global <fittleradam@gmail.com>
2024-02-10 20:54:17 +1000

    basic react rotuing and components setup

--- firstapp/src/components/App.jsx
+++ firstapp/src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { CssBaseLine } from '@mui/material';
-import {Route, Switch } from 'react-router-dom'; 
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom'; 
+import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-        hello this is my app
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+          <Routes>
+              <Route exact path="/" element={<Movies />} />
+              <Route exact path="/movie/:id" element={<MovieInformation />} />
+              <Route exact path="/actors/:id" element={<Actors />} />
+              <Route exact path="/profile/:id" element={<Profile />} />
+          </Routes>
+      </main>
     </div>
   )
 }
